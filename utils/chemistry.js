7041e557e74911d3acfe5fe77b37fc1430f17610
Peter Collingridge <peter.collingridge@gmail.com>
2011-12-10 20:27:42 +0000

    Change chemistry util to store neutron number rather than atomic mass

--- utils/chemistry.js
+++ utils/chemistry.js
@@ -11,20 +11,20 @@ function element( symbol, fullname, protons, neutrons) {
 	this.neutrons = neutrons;
 }
 
-var H  = new element( "H", "hydrogen", 1, [ 1, 2 ] );
-var He = new element( "He", "helium", 2, [ 4, 3 ] );
-var Li = new element( "Li", "lithium", 3, [ 7, 6 ] );
-var Be = new element( "Be", "beryllium", 4, [ 9 ] );
-var F  = new element( "F", "fluorine", 9, [ 19 ] );
-var Ne = new element( "Ne", "neon", 10, [ 20, 22, 21 ] );
-var Na = new element( "Na", "sodium", 11, [ 23 ] );
-var Mg = new element( "Mg", "magnesium", 12, [ 24, 26, 25 ] );
-var Cl = new element( "Cl", "chlorine", 17, [ 35, 37 ] );
-var Ar = new element( "Ar", "argon", 18, [ 40, 36, 38 ] );
-var K  = new element( "K", "potassium", 19, [ 39, 41 ] );
-var Ca = new element( "Ca", "calcium", 20, [ 40, 44, 42, 43, 46 ] );
-var Br = new element( "Br", "bromine", 35, [ 79, 81 ] );
-var Kr = new element( "Kr", "krypton", 36, [ 84, 86, 82, 83, 80, 78 ] );
+var H  = new element( "H", "hydrogen", 1, [ 0, 1 ] );
+var He = new element( "He", "helium", 2, [ 2, 1 ] );
+var Li = new element( "Li", "lithium", 3, [ 4, 3 ] );
+var Be = new element( "Be", "beryllium", 4, [ 5 ] );
+var F  = new element( "F", "fluorine", 9, [ 10 ] );
+var Ne = new element( "Ne", "neon", 10, [ 10, 12, 11 ] );
+var Na = new element( "Na", "sodium", 11, [ 12 ] );
+var Mg = new element( "Mg", "magnesium", 12, [ 12, 14, 13 ] );
+var Cl = new element( "Cl", "chlorine", 17, [ 18, 20 ] );
+var Ar = new element( "Ar", "argon", 18, [ 22, 18, 20 ] );
+var K  = new element( "K", "potassium", 19, [ 20, 22 ] );
+var Ca = new element( "Ca", "calcium", 20, [ 20, 24, 22, 23, 26 ] );
+var Br = new element( "Br", "bromine", 35, [ 44, 46 ] );
+var Kr = new element( "Kr", "krypton", 36, [ 48, 50, 46, 47, 44, 42 ] );
 
 var group1 = [Li, Na, K];
 var group2 = [Be, Mg, Ca];
@@ -32,6 +32,11 @@ var group7 = [F, Cl, Br];
 var group8 = [He, Ne, Ar, Kr];
 var elements = [H].concat( group1, group2, group7, group8 );
 
-alert(elements[3].symbol);
+jQuery.extend(KhanUtil, {
+	// Get a random element
+	randElement: function( ) {
+		return KhanUtil.randFromArray( elements );
+	}
+} );
 
 } )();
