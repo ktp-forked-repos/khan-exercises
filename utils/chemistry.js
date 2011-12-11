3fd0623181a824ec1a9ab12246aa47630168f066
Peter Collingridge <peter.collingridge@gmail.com>
2011-12-11 22:39:38 +0000

    Change containers to chemistryContainers

--- utils/chemistry.js
+++ utils/chemistry.js
@@ -1,7 +1,7 @@
 (function() {
 
 // Elements
-// There's probably a better way to do this, perhaps index by proton number.
+// There's probably a more efficient way to do this, perhaps index by proton number.
 // The attribute 'neutrons' gives the number of nuetrons in stable isotopes, in order of abundance.
 
 function element( protons, symbol, fullname, neutrons) {
@@ -34,6 +34,7 @@ var Ca = new element( 20, "Ca", "calcium", [ 20, 24, 22, 23, 26 ] );
 var Br = new element( 35, "Br", "bromine", [ 44, 46 ] );
 var Kr = new element( 36, "Kr", "krypton", [ 48, 50, 46, 47, 44, 42 ] );
 
+// Groups in the periodic table
 var group1 = [ Li, Na, K ];
 var group2 = [ Be, Mg, Ca ];
 var group3 = [ B, Al ];
@@ -44,12 +45,23 @@ var group7 = [ F, Cl, Br ];
 var group8 = [ He, Ne, Ar, Kr ];
 
 var periodic_groups = [ group1, group2, group3, group4, group5, group6, group7, group8 ];
-var elements = [ H ].concat( group1, group2, group3, group4, group5, group6, group7, group8 );
+var all_elements = [ H ].concat( group1, group2, group3, group4, group5, group6, group7, group8 );
+
+// Molecules
+// Molecules are defined by their molecular formula which is a passed as an array of alternating and numbers
+// e.g. Water is  molecule( [ H, 2, O, 1 ] )
+
+function molecule( components, commonName, molecularFormula) {
+	this.commonName = commonName;
+	this.molecularFormula = molecularFormula;
+}
+
+// H2O = molecule( [ H, 2, O, 1 ] )
 
 jQuery.extend(KhanUtil, {
 	// Get a random element
 	randElement: function( ) {
-		return KhanUtil.randFromArray( elements );
+		return KhanUtil.randFromArray( all_elements );
 	},
 	
 	// Get a random element from a specific group in periodic table
