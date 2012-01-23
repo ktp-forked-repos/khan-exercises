eacf39c407d85c28f0b1aaa82d76a3f2a67b1fbe
Peter Collingridge <peter.collingridge@gmail.com>
2012-01-23 23:06:47 +0000

    Switch to using more compact element object

--- utils/chemistry.js
+++ utils/chemistry.js
@@ -1,9 +1,5 @@
 (function() {
 
-// Elements
-// There must be a more efficient way to do this, to avoid having to generate all the chemicals upfront.
-// The attribute 'neutrons' gives the number of nuetrons in stable isotopes, in order of abundance.
-
 function element( protons, symbol, fullname, neutrons) {
 	this.symbol = symbol;
 	this.fullname = fullname;
@@ -11,17 +7,50 @@ function element( protons, symbol, fullname, neutrons) {
 	this.neutrons = neutrons;
 }
 
-var H  = new element( 1, "H", "hydrogen", [ 0, 1 ] );
-var He = new element( 2, "He", "helium", [ 2, 1 ] );
-var Li = new element( 3, "Li", "lithium", [ 4, 3 ] );
-var Be = new element( 4, "Be", "beryllium", [ 5 ] );
-var B  = new element( 5, "B", "boron", [ 6, 5 ] );
-var C  = new element( 6, "C", "carbon", [ 6, 7 ] );
-var N  = new element( 7, "N", "nitrogen", [ 7, 8 ] );
-var O  = new element( 8, "O", "oxygen", [ 8, 10, 9 ] );
-var F  = new element( 9, "F", "fluorine", [ 10 ] );
-var Ne = new element( 10, "Ne", "neon", [ 10, 12, 11 ] );
-var Na = new element( 11, "Na", "sodium", [ 12 ] );
+var element_objects = {
+	H:  { protons: 1, symbol: "H", name: "hydrogen", mass: 1.008, neutrons: [ 0, 1 ], oxidationStates: [ 1, 0, -1 ] },
+	He: { protons: 2, symbol: "He", name: "helium", mass: 4.003, neutrons: [ 2, 1 ], oxidationStates: [ 0 ] },
+	Li: { protons: 3, symbol: "Li", name: "lithium", mass: 6.941, neutrons: [ 4, 3 ], oxidationStates: [ 0, 1 ] },
+	Be: { protons: 4, symbol: "Be", name: "beryllium", mass: 9.012, neutrons: [ 5 ], oxidationStates: [ 0, 2 ] },
+	B:  { protons: 5, symbol: "B", name: "boron", mass: 10.811, neutrons: [ 6, 5 ], oxidationStates: [ 0, 3 ] },
+	C:  { protons: 6, symbol: "C", name: "carbon", mass: 12.011, neutrons: [ 6, 7 ], oxidationStates: [ -4, 0, 2, 4 ] },
+	N:  { protons: 7, symbol: "N", name: "nitrogen", mass: 14.007, neutrons: [ 7, 8 ], oxidationStates: [ -3, -2, -1, 0, 1, 2, 3, 4, 5 ] },
+	O:  { protons: 8, symbol: "O", name: "oxygen", mass: 15.999, neutrons: [ 8, 10, 9 ], oxidationStates: [ -2, 0 ] },
+	F:  { protons: 9, symbol: "F", name: "fluorine", mass: 18.998, neutrons: [ 10 ], oxidationStates: [ -1, 0 ] },
+	Ne: { protons: 10, symbol: "Ne", name: "neon", mass: 20.180, neutrons: [ 10, 12, 11 ], oxidationStates: [ 0 ] },
+	Na: { protons: 11, symbol: "Na", name: "sodium", mass: 22.990, neutrons: [ 12 ], oxidationStates: [ 0, 1 ] }
+}
+
+var elements = [ "H", "He", "Li", "Be", "B", "C", "N", "O", "F" ];
+
+// --- Compounds ---
+// Compounds are defined by their molecular formula which is a passed as an array of alternating and numbers
+// e.g. Water is  molecule( [ H, 2, O, 1 ] )
+
+function Compound( components, commonName ) {
+	this.commonName = commonName;
+	this.molecularFormula = "";
+	this.mass = 0;
+	
+	for ( var i = 0; i < components.length; i += 2 ) {
+		var element = element_objects[ components[ i ] ];
+		this.mass += element.mass * components[ i + 1 ];
+		this.molecularFormula += element.symbol;
+		if ( components[ i + 1 ] > 1 ) {
+			this.molecularFormula += "_{" + components[ i + 1 ] + "}";
+		}
+	}
+}
+
+var compounds = [
+	new Compound( [ "H", 2 ], "hydrogen gas" ),
+	new Compound( [ "H", 2, "O", 1 ], "water" ),
+	new Compound( [ "C", 1, "O", 2 ], "carbon dioxide" ),
+	new Compound( [ "N", 1, "H", 3 ], "ammonia" ),
+	new Compound( [ "C", 1, "H", 4 ], "methane" ),
+	new Compound( [ "Na", 1, "O", 1, "H", 1 ], "sodium hydroxide" )
+];
+
 var Mg = new element( 12, "Mg", "magnesium", [ 12, 14, 13 ] );
 var Al = new element( 13, "Al", "aluminium", [ 14 ] );
 var Si = new element( 14, "Si", "silicon", [ 14, 15, 16 ] );
@@ -43,6 +72,8 @@ var Pt = new element( 78, "Pt", "platinum", [ 117, 116, 118, 120, 114 ] );
 var Au = new element( 79, "Au", "gold", [ 118 ] );
 
 
+/*
+
 // Groups in the periodic table
 var group1 = [ Li, Na, K ];
 var group2 = [ Be, Mg, Ca ];
@@ -57,49 +88,25 @@ var metals = [ Cu, Ag, Pt, Au ];
 var periodic_groups = [ group1, group2, group3, group4, group5, group6, group7, group8 ];
 var all_elements = [ H ].concat( group1, group2, group3, group4, group5, group6, group7, group8, metals );
 
-// Molecules
-// Molecules are defined by their molecular formula which is a passed as an array of alternating and numbers
-// e.g. Water is  molecule( [ H, 2, O, 1 ] )
-// Atomic mass units assume that molecules consist of the most abundant isotopes
-
-function molecule( components, commonName) {
-	this.commonName = commonName;
-	this.molecularFormula = "";
-	this.amu = 0;
-	
-	for(var i = 0; i < components.length; i += 2) {
-		this.amu += ( components[i].protons +  components[i].neutrons[0] ) * components[ i + 1 ];
-		this.molecularFormula += components[i].symbol;
-		if ( components[ i + 1 ] > 1 ) {
-			this.molecularFormula += "_{" + components[ i + 1 ] + "}";
-		}
-	}
-}
-
-H2 = new molecule( [ H, 2 ], "hydrogen gas" );
-H2O = new molecule( [ H, 2, O, 1 ], "water" );
-CO2 = new molecule( [ C, 1, O, 2 ], "carbon dioxide" );
-NH3 = new molecule( [ N, 1, H, 3 ], "ammonia" );
-CH4 = new molecule( [ C, 1, H, 1 ], "methane" );
 H2SO4 = new molecule( [ H, 2, S, 1, O, 4 ], "sulfuric acid" );
 HCl = new molecule( [ H, 1, Cl, 1 ], "hydrochloric acid" );
-NaOH = new molecule( [ Na, 1, O, 1, H, 1 ], "hydrochloric acid" );
 MgCl2 = new molecule( [ Mg, 1, Cl, 2 ], "magnesium chloride" );
 NaCl = new molecule( [ Na, 1, Cl, 1 ], "sodium chloride" );
 KCl = new molecule( [ K, 1, Cl, 1 ], "potassium chloride" );
 KCN = new molecule( [ K, 1, C, 1, N, 1 ], "potassium cyanide" );
 
 all_compounds = [ H2O, CO2, CH4, H2SO4, HCl, NaOH, MgCl2, NaCl ];
+*/
 
-jQuery.extend(KhanUtil, {
+jQuery.extend( KhanUtil, {
 	// Get a random element
-	randElement: function( ) {
-		return KhanUtil.randFromArray( all_elements );
+	randElement: function() {
+		return element_objects[ KhanUtil.randFromArray( elements ) ];
 	},
 	
 	// Get a random isotope of an element, i.e. it has a specific number of neutrons
 	randIsotope: function( ) {
-		selected_element = KhanUtil.randFromArray( all_elements );
+		selected_element =  element_objects[ KhanUtil.randFromArray( elements ) ];
 		selected_element.neutrons = KhanUtil.randFromArray( selected_element.neutrons );
 		return selected_element;
 	},
@@ -125,7 +132,7 @@ jQuery.extend(KhanUtil, {
 	
 	// Test molecule properties
 	randCompound: function( ) {
-		return KhanUtil.randFromArray( all_compounds );
+		return KhanUtil.randFromArray( compounds );
 	}
 } );
 
